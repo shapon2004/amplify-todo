4d88bb248bc31db6d378a0afa4e76d9682015d1e
Mohammad Ali <shapon2004@hotmail.com>
2020-04-12 17:28:15 +0600

    amplify project with graphql cognito react

---
+++ readme_custom.md
@@ -0,0 +1,10 @@
+1) npm -i -g create-react-app
+2) npm -i -g @aws-amplify@0.1.38
+3) amplify configure
+4) npx create-react-app amplify-notetaker
+5) amplify init
+6) amplify add api
+7) amplify push
+8) npm i aws-amplify aws-amplify-react
+9) amplify console
+10) amplify update api
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './AppHooks';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import aws_export from './aws-exports';
+Amplify.configure(aws_export);
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+ ,
   document.getElementById('root')
 );
 
