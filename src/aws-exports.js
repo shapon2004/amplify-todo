66deca8ebb737812a531ccfa3e44cf4c00bd2848
shapon2004 <shapon2004@hotmail.com>
2020-04-12 17:33:06 +0600

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,11 +2,11 @@
 
 const awsmobile = {
     "aws_project_region": "us-west-2",
-    "aws_cognito_identity_pool_id": "us-west-2:111619e1-4563-44b8-bfed-57bacfe31d18",
-    "aws_cognito_region": "us-west-2",
-    "aws_user_pools_id": "us-west-2_Y9FvYrepN",
-    "aws_user_pools_web_client_id": "24v41e47obsv61t4skvmrh12oi",
-    "aws_appsync_graphqlEndpoint": "https://dvjdmdbfvjgcdnkvsg6tted2yq.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_cognito_identity_pool_id": "us-west-2:111619e1-4563--bfed-57bacfe31d18",
+    "aws_cognito_region": "us--2",
+    "aws_user_pools_id": "us-west-",
+    "aws_user_pools_web_client_id": "",
+    "aws_appsync_graphqlEndpoint": "https://l",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
